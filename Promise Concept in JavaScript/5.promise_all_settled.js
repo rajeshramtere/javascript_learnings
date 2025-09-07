738f9494a76d30d964045fb56f1bfc3f30ab83cc
Rajesh Ramtere <rajesh.ramtere@gmail.com>
2025-09-07 15:09:20 +0530

    Added comments in promise all settled js file

--- Promise Concept in JavaScript/5.promise_all_settled.js
+++ Promise Concept in JavaScript/5.promise_all_settled.js
@@ -1,3 +1,7 @@
+// Promise.allSettled() takes an array of promises and returns a new promise
+// that resolves after all of the input promises have settled (either fulfilled or rejected).
+// The result is an array of objects describing the outcome of each promise.
+
 const function4 = () => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
@@ -14,6 +18,8 @@ const function5 = () => {
     });
 };
 
+// Here, function4 resolves and function5 rejects after 1 second.
+// Promise.allSettled will wait for both to finish and then provide their results.
 Promise.allSettled([
     function4(),
     function5()
